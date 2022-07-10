5ce13099ec38b533aee75d5516a9f21fe836a018
DoguYllmaz <dogu_rog@hotmail.com>
2022-07-10 03:23:06 +0300

    .

--- src/main/java/com/example/bookstore/repository/WriterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookstore.repository;
-
-import com.example.bookstore.model.Writer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WriterRepository extends JpaRepository<Writer,Long> {
-}
